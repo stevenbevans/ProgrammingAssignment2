e93bbe3d7cbc0e31110839692dc0b05690cc106a
Steven Evans <stevenbevans@gmail.com>
2014-07-23 21:08:34 +0200

    added test function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Solution for Coursera R assignment 2
+##Create cache matrix inverse function  
 
-## Write a short comment describing this function
+
+## Function  to create a cache matrix 
+## sample usage  makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2)) 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(amatrix) {
+        x <<- amatrix
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function(){ 
+        m                   
+    }
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse )
+  
 }
 
 
-## Write a short comment describing this function
+## Get the the cached matrix or create it if it does not exist
+## sample usage a <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2)) 
+## cacheSolve(a)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    cachem <- x$getInverse()
+  ##Check if already cached    
+    if(!is.null(cachem)) {
+        message("getting cached data")
+        return(cachem)
+    }
+    data <- x$get()
+    cachem <- solve(data, ...)
+    x$setInverse(cachem)
+    cachem 
+    
+  ## Return a matrix that is the inverse of 'x'
+}
+
+## Used for testing
+testmatrixcacheinverter <- function(){
+    am = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+    print(am$get()) 
+    print(cacheSolve(am))
+    print(cacheSolve(am))##check to make sure using cache matrix
+    am$getInverse()
 }
